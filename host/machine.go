fc700ad94083b167db89132266309c68254a1929
Damon Kelley <damon.kelley@gmail.com>
2015-06-01 00:45:39 -0500

    Make the machine module more generic by renaming it to host.

---
+++ host/machine.go
@@ -0,0 +1,22 @@
+package host
+
+import (
+	"crypto/md5"
+	"fmt"
+)
+
+type Host struct {
+	Id, Hostname, IP string
+}
+
+func NewHost(hostname, ip string) Host {
+	id := createHostId(hostname)
+
+	return Host{id, hostname, ip}
+}
+
+func createHostId(hostname string) string {
+	id := []byte(hostname)
+
+	return fmt.Sprintf("%x", md5.Sum(id))
+}
