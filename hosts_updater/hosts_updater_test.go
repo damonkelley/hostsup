5c3fcb6a99763d555476f17d96db0045494056bb
Damon Kelley <damon.kelley@gmail.com>
2015-06-04 23:42:33 -0500

    Test for ListHostsEntries.

--- hosts_updater/hosts_updater_test.go
+++ hosts_updater/hosts_updater_test.go
@@ -51,3 +51,21 @@ func TestRemoveHostsEntry(t *testing.T) {
 
 	t.Error(fmt.Sprintf("Expected to find %s in testdata/hosts", hostname))
 }
+
+func TestListHostsEntries(t *testing.T) {
+	hostname1, ip1 := "dev1.dev", "192.168.0.1"
+	host1 := host.NewHost(hostname1, ip1)
+
+	hostname2, ip2 := "dev2.dev", "192.168.0.2"
+	host2 := host.NewHost(hostname2, ip2)
+
+	h := NewHostsfile("testdata/hosts")
+	h.AddHostsEntry(host1)
+	h.AddHostsEntry(host2)
+
+	hosts := h.ListHostsEntries()
+
+	if len(hosts) != 2 {
+		t.Error(fmt.Sprintf("Expected to find 2 host entry. Found %d instead.", len(hosts)))
+	}
+}
