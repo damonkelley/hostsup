80162e28da232f3daf957964d38ee5f2f8153097
Damon Kelley <damon.kelley@gmail.com>
2015-06-08 19:25:17 -0500

    Add a test for NewHost.

--- hostsfile/host_test.go
+++ hostsfile/host_test.go
@@ -1,1 +1,16 @@
 package hostsfile
+
+import (
+	"testing"
+)
+
+func TestNewHost(t *testing.T) {
+	host := NewHost("IP", "hostname")
+
+	id := createHostId("hostname")
+	stub := Host{"IP", "hostname", id}
+
+	if *host != stub {
+		t.Error("Ordering of Host fields do not match.")
+	}
+}
